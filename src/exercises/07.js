3811db1a1274c31be6d1d7b8c804083f901d5961
dmytro.zhyrov <dmytro.zhyrov@sinnerschrader.com>
2020-11-06 23:05:54 +0100

    props collection, props getter, state initializer

--- src/exercises/07.js
+++ src/exercises/07.js
@@ -7,16 +7,15 @@ const callAll = (...fns) => (...args) =>
   fns.forEach(fn => fn && fn(...args))
 
 class Toggle extends React.Component {
-  // 🐨 We're going to need some static defaultProps here to allow
-  // people to pass a `initialOn` prop.
-  //
-  // 🐨 Rather than initializing state to have on as false,
-  // set on to this.props.initialOn
-  state = {on: false}
+  static defaultProps = {
+    initialOn: false
+  }
 
-  // 🐨 now let's add a reset method here that resets the state
-  // to the initial state. Then add a callback that calls
-  // this.props.onReset with the `on` state.
+  state = {on: this.props.initialOn}
+
+  reset = () => {
+    this.setState({on: this.props.initialOn}, () => this.props.onReset(this.state.on))
+  }
   toggle = () =>
     this.setState(
       ({on}) => ({on: !on}),
@@ -29,12 +28,12 @@ class Toggle extends React.Component {
       ...props,
     }
   }
+
   getStateAndHelpers() {
     return {
       on: this.state.on,
       toggle: this.toggle,
-      // 🐨 now let's include the reset method here
-      // so folks can use that in their implementation.
+      reset: this.reset,
       getTogglerProps: this.getTogglerProps,
     }
   }
